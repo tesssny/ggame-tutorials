84baea7907f187ca3e0d4b7063e40ae41318d3c8
tesssny <teresasnyder@hanovernorwichschools.org>
2016-03-07 14:20:11 -0500

    Updated from Brython Server: 3/7/2016 2:20:11 PM

--- tutorial4.py
+++ tutorial4.py
@@ -15,5 +15,15 @@ class SpaceGame(App):
         bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, black)
         bg = Sprite(bg_asset, (0,0))
 
+class SpaceShip(Sprite):
+    """
+    Animated space ship
+    """
+    asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
+        Frame(227,0,292-227,125), 4, 'vertical')
+
+    def __init__(self, position):
+        super().__init__(SpaceShip.asset, position)
+
 myapp = SpaceGame(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.run()
