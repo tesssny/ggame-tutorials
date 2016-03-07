6b5e7026b1ea064a142c5ac0c99bd87200c90a9d
tesssny <teresasnyder@hanovernorwichschools.org>
2016-03-07 14:40:50 -0500

    Updated from Brython Server: 3/7/2016 2:40:49 PM

--- tutorial4.py
+++ tutorial4.py
@@ -3,6 +3,45 @@ from ggame import App, RectangleAsset, ImageAsset, Sprite, LineStyle, Color, Fra
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 
+class SpaceShip(Sprite):
+    """
+    Animated space ship
+    """
+    asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
+        Frame(227,0,292-227,125), 4, 'vertical')
+
+    def __init__(self, position):
+        super().__init__(SpaceShip.asset, position)
+        self.vx = 1
+        self.vy = 1
+        self.vr = 0.01
+        
+        self.thrust = 0
+        self.thrustframe = 1
+        if self.thrust == 1:
+            self.setImage(self.thrustframe)
+            self.thrustframe += 1
+        if self.thrustframe == 4:
+            self.thrustframe = 1
+        else:
+            self.setImage(0)
+    
+        SpaceGame.listenKeyEvent("keydown", "space", self.thrustOn)
+        SpaceGame.listenKeyEvent("keyup", "space", self.thrustOff)
+        self.fxcenter = self.fycenter = 0.5
+        
+        def step(self):
+            for ship in self.getSpritesbyClass(SpaceShip):
+                ship.step()
+        
+        def step(self):
+            self.x += self.vx
+            self.y += self.vy
+            self.rotation += self.vr
+        def thrustOn(self, event):
+            self.thrust = 1
+        def thrustOff(self, event):
+            self.thrust = 0
 
 class SpaceGame(App):
     """
@@ -22,28 +61,6 @@ class SpaceGame(App):
             for ship in self.getSpritesbyClass(SpaceShip):
                 ship.step()
 
-class SpaceShip(Sprite):
-    """
-    Animated space ship
-    """
-    asset = ImageAsset("images/four_spaceship_by_albertov_with_thrust.png", 
-        Frame(227,0,292-227,125), 4, 'vertical')
-
-    def __init__(self, position):
-        
-        def step(self):
-            for ship in self.getSpritesbyClass(SpaceShip):
-                ship.step()
-        
-        def step(self):
-            self.x += self.vx
-            self.y += self.vy
-            self.rotation += self.vr
-        
-        super().__init__(SpaceShip.asset, position)
-        self.vx = 1
-        self.vy = 1
-        self.vr = 0.01
 
 myapp = SpaceGame(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.run()
